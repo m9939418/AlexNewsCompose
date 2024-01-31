93144cf61db90b367f26523ec4ce70b5a9e4f0ca
AlexYang <m9939418@gmail.com>
2024-01-31 21:02:58 +0800

    #3 add OnBoarding Page

---
+++ app/src/main/java/com/alex/yang/alexnewscompose/presentation/onboading/components/PagerIndicator.kt
@@ -0,0 +1,46 @@
+package com.alex.yang.alexnewscompose.presentation.onboading.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.alex.yang.alexnewscompose.presentation.onboading.Dimens.IndicatorSize
+import com.alex.yang.alexnewscompose.ui.theme.AlexNewsComposeTheme
+import com.alex.yang.alexnewscompose.ui.theme.BlueGray
+
+
+@Composable
+fun PagerIndicator(
+    modifier: Modifier = Modifier,
+    pageSize: Int,
+    selectedPage: Int,
+    selectedColor: Color = MaterialTheme.colorScheme.primary,
+    unselectedColor: Color = BlueGray,
+) {
+    Row(modifier = modifier, horizontalArrangement = Arrangement.SpaceBetween) {
+        repeat(pageSize) { page ->
+            Box(
+                modifier = Modifier
+                    .size(IndicatorSize)
+                    .clip(CircleShape)
+                    .background(color = if (page == selectedPage) selectedColor else unselectedColor)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PagerIndicatorPreview(){
+    AlexNewsComposeTheme {
+        PagerIndicator(pageSize = 5, selectedPage = 2)
+    }
+}
